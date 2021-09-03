248923eba493602b3135c6df606fb4ae6c8e5315
akshay kumar <akshay@geekconsults.co.in>
2021-09-03 23:48:27 +0530

    mvp

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,3 +25,92 @@ fetch("./includes/footer.html")
     catch(e){}
 });
 
+/* Alpine JS - State control */
+
+const STATUS = {
+  idle:"0",
+  loading:"1",
+  error:"2"
+}
+function getData(){
+  return {
+      student_name:"",
+      cert_auth:"",
+      program_id:"",
+      create_status:STATUS.idle,
+      async createCert() {
+        console.log("ic.create called")
+        this.create_status = STATUS.loading
+        let api = "https://solana-deploy.parmu.town/registerCertificate"
+        const data = {
+          "student-name":this.student_name,
+          "certifying-authority":this.cert_auth
+        }
+        const rawResponse  = await fetch(api,{
+          // mode:"no-cors",
+          headers:{
+            'Accept': 'application/json',
+            "Content-Type":"application/json"
+          },
+          method:"POST",
+          body: JSON.stringify(data)
+        })
+        const result = await rawResponse.text();
+        let resultJson = ""
+        try{
+          resultJson = JSON.parse(result)
+          this.program_id = resultJson.result
+          this.create_status = STATUS.idle
+          if(resultJson.status == 200){
+            $('#create-modal form')[0].reset()
+            $('#create-modal').removeClass("modal-open")
+            $('#show-modal').toggleClass('modal-open')
+          }
+        }
+        catch(e){}
+
+        this.create_status = STATUS.idle
+
+        // this.data
+        
+      },
+      
+      copyId() {
+        var type = "text/plain";
+        var blob = new Blob([this.program_id], { type });
+        var data = [new ClipboardItem({ [type]: blob })];
+    
+        navigator.clipboard.write(data).then(
+            function () {
+              /* success */
+              Snackbar.show({ 
+                showAction: true,
+                actionTextColor:"#424242",
+                actionText:"OK",
+                text:"Copied to clipboard.",
+                pos:"bottom-center",
+                backgroundColor:"#fff",
+                textColor:"#000"
+               });
+
+            },
+            function () {
+            /* failure */
+            }
+        );
+      },
+      get explorerUrl(){
+        return `https://explorer.solana.com/address/${this.program_id}?cluster=devnet`;
+      },
+      toggleCreateModal(){
+        $('#create-modal').toggleClass("modal-open")
+      },
+      toggleShowModal(){
+        $('#show-modal').toggleClass("modal-open")
+      },
+      toggleVerifyModal(){
+        $('#verify-modal').toggleClass("modal-open")
+      }
+
+  }
+}
